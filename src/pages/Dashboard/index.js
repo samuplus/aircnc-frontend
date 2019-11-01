c323dc5b43c66146b0b9a8cab3f776cfcc461de5
samuplus <samuplus@hotmail.com>
2019-11-01 00:09:54 +0000

    Updated dotenv file

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -10,7 +10,7 @@ export default function Dashboard(){
     const [requests, setRequests] = useState([]);
 
     const user_id = localStorage.getItem('user');
-    const socket = useMemo(() => socketio('http://localhost:3333', {
+    const socket = useMemo(() => socketio(process.env.REACT_APP_API_URL, {
         query: { user_id }
     }), [user_id]);
 
