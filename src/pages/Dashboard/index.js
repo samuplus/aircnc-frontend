e55073d7f4385eb0a56bbd041bdfeb29fe7eb06f
samuplus <samuplus@hotmail.com>
2019-11-01 17:09:57 +0000

    Modified Dashboard page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -68,7 +68,7 @@ export default function Dashboard(){
             <ul className="spot-list">
                 {spots.map(spot => (
                     <li key={spot._id}>
-                        <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }} />
+                        <header style={{ backgroundImage: `url(${spot.url})` }} />
                         <strong>{spot.company}</strong>
                         <span>{spot.price ? `£${spot.price} per day` : 'FREE' }</span>
                     </li>
